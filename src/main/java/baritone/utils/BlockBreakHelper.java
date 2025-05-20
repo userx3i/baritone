330fa879a3f64253766cd531a6789f456dfe0de2
smorbes <isaacaforbes@gmail.com>
2025-05-20 04:57:35 -0700

    Push

---
+++ src/main/java/baritone/utils/BlockBreakHelper.java
@@ -0,0 +1,83 @@
+/*
+ * This file is part of Baritone.
+ *
+ * Baritone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Baritone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Baritone.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package baritone.utils;
+
+import baritone.api.BaritoneAPI;
+import baritone.api.utils.IPlayerContext;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+
+/**
+ * @author Brady
+ * @since 8/25/2018
+ */
+public final class BlockBreakHelper {
+    // base ticks between block breaks caused by tick logic
+    private static final int BASE_BREAK_DELAY = 2;
+
+    private final IPlayerContext ctx;
+    private boolean didBreakLastTick;
+    private int breakDelayTimer = 0;
+
+    BlockBreakHelper(IPlayerContext ctx) {
+        this.ctx = ctx;
+    }
+
+    public void stopBreakingBlock() {
+        // The player controller will never be null, but the player can be
+        if (ctx.player() != null && didBreakLastTick) {
+            if (!ctx.playerController().hasBrokenBlock()) {
+                // insane bypass to check breaking succeeded
+                ctx.playerController().setHittingBlock(true);
+            }
+            ctx.playerController().resetBlockRemoving();
+            didBreakLastTick = false;
+        }
+    }
+
+    public void tick(boolean isLeftClick) {
+        if (breakDelayTimer > 0) {
+            breakDelayTimer--;
+            return;
+        }
+        HitResult trace = ctx.objectMouseOver();
+        boolean isBlockTrace = trace != null && trace.getType() == HitResult.Type.BLOCK;
+
+        if (isLeftClick && isBlockTrace) {
+            if (!didBreakLastTick) {
+                ctx.playerController().syncHeldItem();
+                ctx.playerController().clickBlock(((BlockHitResult) trace).getBlockPos(), ((BlockHitResult) trace).getDirection());
+                ctx.player().swing(InteractionHand.MAIN_HAND);
+            }
+
+            // Attempt to break the block
+            if (ctx.playerController().onPlayerDamageBlock(((BlockHitResult) trace).getBlockPos(), ((BlockHitResult) trace).getDirection())) {
+                ctx.player().swing(InteractionHand.MAIN_HAND);
+            }
+
+            ctx.playerController().setHittingBlock(false);
+
+            didBreakLastTick = true;
+        } else if (didBreakLastTick) {
+            stopBreakingBlock();
+            breakDelayTimer = BaritoneAPI.getSettings().blockBreakSpeed.value - BASE_BREAK_DELAY;
+            didBreakLastTick = false;
+        }
+    }
+}
---
+++ src/main/java/baritone/utils/PlayerMovementInput.java
@@ -0,0 +1,67 @@
+/*
+ * This file is part of Baritone.
+ *
+ * Baritone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Baritone is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Baritone.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package baritone.utils;
+
+import baritone.api.utils.input.Input;
+import net.minecraft.client.player.ClientInput;
+
+public class PlayerMovementInput extends ClientInput {
+
+    private final InputOverrideHandler handler;
+
+    PlayerMovementInput(InputOverrideHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void tick() {
+        this.leftImpulse = 0.0F;
+        this.forwardImpulse = 0.0F;
+        boolean jumping = handler.isInputForcedDown(Input.JUMP); // oppa gangnam style
+
+        boolean up = handler.isInputForcedDown(Input.MOVE_FORWARD);
+        if (up) {
+            this.forwardImpulse++;
+        }
+
+        boolean down = handler.isInputForcedDown(Input.MOVE_BACK);
+        if (down) {
+            this.forwardImpulse--;
+        }
+
+        boolean left = handler.isInputForcedDown(Input.MOVE_LEFT);
+        if (left) {
+            this.leftImpulse++;
+        }
+
+        boolean right = handler.isInputForcedDown(Input.MOVE_RIGHT);
+        if (right) {
+            this.leftImpulse--;
+        }
+
+        boolean sneaking = handler.isInputForcedDown(Input.SNEAK);
+        if (sneaking) {
+            this.leftImpulse *= 0.3D;
+            this.forwardImpulse *= 0.3D;
+        }
+
+        boolean sprinting = handler.isInputForcedDown(Input.SPRINT);
+
+        this.keyPresses = new net.minecraft.world.entity.player.Input(up, down, left, right, jumping, sneaking, sprinting);
+    }
+}
